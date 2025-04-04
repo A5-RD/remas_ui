fd890df87b7c6bbf57e59ecca02c3aa7fc152f29
a5eng <orvill.delatorre@atom5engineering.com>
2025-04-04 07:36:22 -0400

    -

--- firebase.js
+++ firebase.js
@@ -7,11 +7,11 @@ const firebaseConfig = {
     apiKey: "AIzaSyA9WkNeMgcrgmhUA97UPbp7R4pj-IZFnK0",
     authDomain: "atom5engineering.firebaseapp.com",
     projectId: "atom5engineering",
-    storageBucket: "atom5engineering.appspot.com",
+    storageBucket: "atom5engineering.firebasestorage.app",
     messagingSenderId: "956458197323",
     appId: "1:956458197323:web:e01873bb7fa92ac70a08ce",
     measurementId: "G-X7H184TN54"
-};
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
