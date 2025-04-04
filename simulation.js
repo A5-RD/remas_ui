7a9e43994787cf1d0ab5664b2cfb86c67e2451e0
a5eng <orvill.delatorre@atom5engineering.com>
2025-04-04 07:34:47 -0400

    -

--- simulation.js
+++ simulation.js
@@ -2,6 +2,7 @@
 import { storage , auth} from "./firebase.js";  // Import Firebase Storage
 import { ref, listAll, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-storage.js";  // Import storage functions
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js"; // Import Auth function
+console.log(storage)
 
 
 
